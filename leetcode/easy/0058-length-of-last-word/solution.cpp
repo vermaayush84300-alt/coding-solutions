7995a02ea4460ea1a8c48eac28c1e5d5842a5bbe
Ayush Kumar <vermaayush84300@gmail.com>
2026-09-06 23:53:41 +0530

    ✅ Length of Last Word | Easy | C++ | Runtime: 0 ms (100.00%) | Memory: 8.8 MB | 2026-09-06

---
+++ leetcode/easy/0058-length-of-last-word/solution.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int i = s.size() -1;
+        while(i>=0 && s[i] ==' '){
+            i--;
+        }
+            int length =0;
+            while(i>=0 && s[i] !=' '){
+                length++;
+                i--;
+            }
+            return length;
+        
+       
+    }
+};
